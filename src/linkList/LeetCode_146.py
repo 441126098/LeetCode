9c4f4b15b53250f2ef7656ac107cd1cf8f53e5c3
allenZhang <allenzhang570@gmail.com>
2025-06-08 11:46:14 +0800

    优化 LRUCache 实现，使用 OrderedDict 替代原有字典和列表结构

--- src/linkList/LeetCode_146.py
+++ src/linkList/LeetCode_146.py
@@ -1,24 +1,26 @@
+from collections import OrderedDict
+
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.value = {}
-        self.list = []
+        self.cache = OrderedDict()
         
 
     def get(self, key: int) -> int:
-        if key in self.value:
-            val = self.value[key]
-            self.list.remove(key)
-            self.list.append(key)
-            return val
-        return -1
+        if key not in self.cache:
+            return -1
+        # 移动到末尾（标记为最近使用）
+        self.cache.move_to_end(key)
+        return self.cache[key]
 
     def put(self, key: int, value: int) -> None:
-        if key in self.value:
-            self.list.remove(key)
-        elif len(self.value) >= self.capacity:
-            old_key = self.list.pop(0)
-            del self.value[old_key]
-        self.list.append(key)
-        self.value[key] = value
+        if key in self.cache:
+            # 更新值并移动到末尾
+            self.cache[key] = value
+            self.cache.move_to_end(key)
+        else:
+            if len(self.cache) >= self.capacity:
+                # 移除最久未使用的项（第一个）
+                self.cache.popitem(last=False)
+            self.cache[key] = value
